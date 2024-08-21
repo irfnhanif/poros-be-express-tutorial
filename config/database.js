f99ef1f557c80c08060e00b9b87d5ba396cbfe7a
irfnhanif <14.irfanh@gmail.com>
2024-08-21 11:41:36 +0700

    feat: Add database configuration file

---
+++ config/database.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+  },
+);
+
+export default sequelize;
