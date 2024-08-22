938a71c47018da911f0bad42479186305514494d
irfnhanif <14.irfanh@gmail.com>
2024-08-22 20:52:40 +0700

    chore: Update app.js to use posts routes instead of users routes

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 import express from "express";
-import indexRouter from "./routes/index.js";
-import usersRouter from "./routes/users.js";
+import "dotenv/config";
+import postRouter from "./routes/posts.routes.js";
+// import usersRouter from "./routes/users.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/posts", postRouter);
+// app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Server is running on port ${process.env.APP_PORT}`);
 });
 
 export default app;
